a99826dd3907f5852c7b5c515aa406d3db14db0b
Kurt Scherer <kurt.scherer@gmail.com>
2017-03-07 12:43:48 -0600

    Adding Client

---
+++ client.go
@@ -0,0 +1,29 @@
+package statuspage
+
+import (
+	"net/http"
+	"net/url"
+	"time"
+)
+
+const DefaultAPIURL = "https://api.statuspage.io/v1/"
+
+type Client struct {
+	apiKey     string
+	pageID     string
+	httpClient *http.Client
+	url        *url.URL
+}
+
+func NewClient(apiKey, pageID string) *Client {
+	u, err := url.Parse(DefaultAPIURL)
+	if err != nil {
+		panic(err)
+	}
+	return &Client{
+		apiKey:     apiKey,
+		pageID:     pageID,
+		httpClient: &http.Client{Timeout: 5 * time.Second},
+		url:        u,
+	}
+}
