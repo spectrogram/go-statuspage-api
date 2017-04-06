263485b5b247b681142df15a8918f2e7ef6e7e7b
Christopher 'Chief' Najewicz <chief@beefdisciple.com>
2017-04-06 11:42:13 -0400

    Adding ability to update component's name/description/status as per API Adding Makefile Temporarily removing kr/pretty

--- component.go
+++ component.go
@@ -2,9 +2,8 @@ package statuspage
 
 import (
 	"fmt"
+	"net/url"
 	"time"
-
-	"github.com/kr/pretty"
 )
 
 type Component struct {
@@ -19,15 +18,36 @@ type Component struct {
 	GroupID     *string    `json:"group_id,omitempty"`
 }
 
-type ComponentResponse []Component
+func (c *Component) String() string {
+	var out string
+	line := "-----------------"
+	out = fmt.Sprintf("\n%s\nCreated: %s\nName: %s\nID: %s\nStatus: %s\n%s\n",
+		line,
+		*c.CreatedAt,
+		*c.Name,
+		*c.ID,
+		*c.Status,
+		line,
+	)
+	return out
+}
+
+type ComponentUpdateData struct {
+	Data string
+}
+
+func (c *ComponentUpdateData) String() string {
+	return c.Data
+}
+
+type ComponentsResponse []Component
 
 func (c *Client) doGetComponents(path string) ([]Component, error) {
-	resp := ComponentResponse{}
+	resp := ComponentsResponse{}
 	err := c.doGet(path, nil, &resp)
 	if err != nil {
 		return nil, err
 	}
-	pretty.Println(resp)
 	return resp, nil
 }
 
@@ -59,4 +79,46 @@ func (c *Client) GetComponentByName(name string) (*Component, error) {
 		}
 	}
 	return nil, fmt.Errorf("search error: Component with name %s not found", name)
+}
+
+func (c *Client) doUpdateComponent(comp *Component, params fmt.Stringer) (*Component, error) {
+	resp := Component{}
+	path := fmt.Sprintf("components/%s.json", *comp.ID)
+	err := c.doPatch(path, params, &resp)
+	if err != nil {
+		return nil, err
+	}
+	return &resp, nil
+}
+
+func (c *Client) updateComponent(comp *Component, attr string) (*Component, error) {
+	var newVal string
+	switch attr {
+	case "name":
+		newVal = *comp.Name
+	case "description":
+		newVal = *comp.Description
+	case "status":
+		newVal = *comp.Status
+	}
+	params := &ComponentUpdateData{
+		Data: fmt.Sprintf("component[%s]=%s", attr, url.QueryEscape(newVal)),
+	}
+	uc, err := c.doUpdateComponent(comp, params)
+	if err != nil {
+		return nil, err
+	}
+	return uc, nil
+}
+
+func (c *Client) UpdateComponentName(comp *Component) (*Component, error) {
+	return c.updateComponent(comp, "name")
+}
+
+func (c *Client) UpdateComponentStatus(comp *Component) (*Component, error) {
+	return c.updateComponent(comp, "status")
+}
+
+func (c *Client) UpdateComponentDesc(comp *Component) (*Component, error) {
+	return c.updateComponent(comp, "description")
 }
